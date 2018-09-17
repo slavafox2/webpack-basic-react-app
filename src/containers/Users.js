507387d7dd38d6521c74a87eb03c5b5ebda670ab
slavafox2 <tyop@tut.by>
2018-09-17 17:34:09 +0300

    add new

---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+
+class Users extends Component {
+    render  () {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>Awesome Users on board of this course.</p>
+            </div>
+        );
+    };
+};
+
+export default Users;
